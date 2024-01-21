6c2d9d6a4f2e392454d7a51baa162d8019912fa4
Kraiviks <kraiviks@gmail.com>
2024-01-21 14:17:39 +0200

    updated descriptions for projects

--- src/js/description.js
+++ src/js/description.js
@@ -1,22 +1,22 @@
 export const data = {
 	'd-etf': {
 		title: 'D-ETF',
-		text: "For project D-ETF, Implemented of the authorization system, cryptocurrency buying/selling functionality, and an administrator panel where registered users and transactions can be managed. Additionally, I implemented the ability to change the website's localization using react-18next, which increased the number of users.",
+		text: "D-ETF is providing the future infrastructure of finance by bringing ETFs on to the blockchain. For project D-ETF, Implemented of the authorization system, cryptocurrency buying/selling functionality, and an administrator panel where registered users and transactions can be managed. Additionally, I implemented the ability to change the website's localization using react-18next, which increased the number of users.",
 		tags: ['JavaScript', 'REACT', 'HTML', 'CSS', 'SASS', 'REDUX'],
 	},
 	'first bridge': {
 		title: 'First Bridge',
-		text: 'Redesigning existing pages and creating new ones, updating old components, and developing new elements. Project optimization resulted in a productivity increase from 60% to 80% and beyond.',
+		text: 'First Bridge is an R&D company that provides technology consulting services and creates outstanding solutions based on the most ambitious ideas. For this project, I redesigned existing pages and created new ones, updated old components, and developed new elements. I created a template for quick and convenient addition of new articles. Project optimization resulted in a productivity increase from 60% to 80% and beyond.',
 		tags: ['JavaScript', 'REACT', 'NEXT.JS', 'HTML', 'SASS'],
 	},
 	'the grid': {
 		title: 'The Grid',
-		text: 'Revamping existing pages, creating new ones, updating old components, and developing new elements.',
+		text: 'Southeast Asia & ANZ Sales Intelligence Platform. For this project, I developed the "Applied Filters Panel" for the /filter page, replacing the old react-table with ag-grid-react for more convenient table development. I also implemented a banner system, incorporating user-friendly tooltips using react-tooltip. Animated images were added using lottie-react. I redesigned existing pages, created new ones, updated old components, and developed new elements.',
 		tags: ['JavaScript', 'REACT', 'NEXT.JS', 'HTML', 'SASS', 'CSS Modules'],
 	},
 	polar: {
 		title: 'Polar',
-		text: 'Updating existing components and developing new ones.',
+		text: 'Landing: POLAR utilizes proprietary technology and a unique approach to create the next generation multimedia codecs family. For this project, I updated existing components and developed new ones. Enhanced the table and implemented responsiveness for it.',
 		tags: ['TypeScript', 'REACT', 'HTML', 'SASS'],
 	},
 };
